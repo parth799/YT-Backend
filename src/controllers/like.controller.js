836dab8cf7c3188f99c1bb97e7e3ed5fa4022fab
parth799 <118920131+parth799@users.noreply.github.com>
2024-08-22 10:16:07 +0530

    feat: Add function and route to fetch liked videos

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -28,4 +28,64 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, { likedBy: true }, "Liked successfully"));
 })
 
-export { toggleVideoLike }
+const getLikedVideos = asyncHandler(async(req, res) => {
+    const likedVideosAgg = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id),
+            },
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideo",
+                pipeline: [
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"ownerDetails"
+                        }
+                    },
+                    {
+                        $unwind:"$ownerDetails"
+                    }
+                ]
+            },
+        },
+        {
+            $unwind:"$likedVideo"
+        },
+        {
+            $sort:{
+                createdAt: -1,
+            },
+        },
+        {
+            $project:{
+                _id: 0,
+                likedVideo:{
+                    _id: 1,
+                    videoFile: 1,
+                    thumbnail: 1,
+                    owner: 1,
+                    title: 1,
+                    description: 1,
+                    duration: 1,
+                    createdAt: 1,
+                    isPublished:1,
+                    ownerDetails:{
+                        username: 1,
+                        fullName: 1,
+                        avatar: 1
+                    }
+                }
+            }
+        }
+    ]);
+    return res.status(200).json(new ApiResponse(200, likedVideosAgg,"liked videos fetched successfully!f"));
+})
+export { toggleVideoLike ,getLikedVideos}
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import { toggleVideoLike,getLikedVideos } from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/toggle/v/:videoId").post(toggleVideoLike)
+router.route("/videos").get(getLikedVideos);
 
 export default router;
