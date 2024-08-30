f4f67eff1deb6dfb340c37be62d8e383cccf8244
parth799 <118920131+parth799@users.noreply.github.com>
2024-08-30 10:35:02 +0530

    refactor: Rename routes for consistency and clarity.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -37,16 +37,16 @@ router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
-router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/update-user").patch(verifyJWT, updateAccountDetails);
 
 router
-  .route("/avatar")
+  .route("/update-avatar")
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
-  .route("/cover-image")
+  .route("/update-coverImg")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
-router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
-router.route("/history").get(verifyJWT, getWatchHistory);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
