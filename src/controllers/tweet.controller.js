54bb46acc48dff7ea12bb096ec2eab1dc163928c
parth799 <118920131+parth799@users.noreply.github.com>
2024-09-05 16:22:51 +0530

    feat: Add update tweet functionality and route

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import {Tweet} from "../models/tweet.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { isValidObjectId } from "mongoose";
 
 const createTweet = asyncHandler(async (req,res) => {
     const {content} = req.body;
@@ -22,4 +23,26 @@ const createTweet = asyncHandler(async (req,res) => {
     return res.status(201).json(new ApiResponse(201, tweet, "Tweet created successfully!"));
 })
 
-export { createTweet } 
+const updateTweet = asyncHandler(async(req,res) => {
+    const {tweetId} = req.params;
+    const {content} = req.body;
+    
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet id");
+    }
+
+    if(!content){
+        throw new ApiError(400, "Content cannot be empty");
+    }
+    
+    const tweet = await Tweet.findByIdAndUpdate(tweetId, {content}, {new: true, runValidators: true});
+
+    if(!tweet){
+        throw new ApiError(404, "Tweet not found");
+    }
+
+    return res.status(200).json(new ApiResponse(200, tweet, "Tweet updated successfully!"));
+})
+
+
+export { createTweet,updateTweet } 
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, updateTweet } from "../controllers/tweet.controller.js";
 
 
 const router = Router();
 
 
 router.route("/").post(verifyJWT, createTweet)
+router.route("/:tweetId").patch(verifyJWT, updateTweet)
 
 export default router;
